fcf957f213c58fd400dabdb74eb367beecd10738
siddharth9442 <siddharthkhubikar9442@gmail.com>
2024-03-22 21:46:53 +0530

    update database connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
+import app from "./app.js";
 import connectDB from "./db/index.js";
 import 'dotenv/config.js'
 
+const PORT = process.env.PORT || 8000;
+
 connectDB()
+.then( () => {
+    app.listen( PORT, () => console.log(`Server is running at port: ${PORT}`))
+} )
+.catch(err => {
+    console.log("MONGO db connection failed !!", err);
+})
